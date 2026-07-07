b55ab75aa7796ecb4c3918009fe29637299e9ffe
hubertyameogo91-svg <hubertyameogo91@gmail.com>
2026-07-07 00:06:26 +0000

    Update index.js

--- index.js
+++ index.js
@@ -5,9 +5,8 @@ const { RtcTokenBuilder, RtcRole } = require('agora-access-token');
 const app = express();
 app.use(cors());
 
-const APP_ID = "3e6cc8398f83416ab70c61a957f5477a";
-const APP_CERTIFICATE = "b24e99ae5f05409a842df8889a802313";
-
+const APP_ID = process.env.AGORA_APP_ID;
+const APP_CERTIFICATE = process.env.AGORA_APP_CERTIFICATE;
 app.get('/token', (req, res) => {
     const channelName = req.query.channelName;
     if (!channelName) {
